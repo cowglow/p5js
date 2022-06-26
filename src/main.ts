6826de744214a0a7efce0abb1be43343dd68837f
Philip Saa <cowglow@gmail.com>
2022-06-26 16:31:16 +0200

    randomize  sketch selection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./style.css";
-// import sketch from "./sketches/prototype-001.js";
-import sketch from "./sketches/prototype-002.js";
+import sketch1 from "./sketches/prototype-001.js";
+import sketch2 from "./sketches/prototype-002.js";
 import p5 from "p5";
 
+const sketches = [sketch1, sketch2];
+
 const targetElement = document.querySelector<HTMLDivElement>("#container")!;
-new p5(sketch, targetElement);
+new p5(sketches[Math.floor(Math.random() * sketches.length)], targetElement);
