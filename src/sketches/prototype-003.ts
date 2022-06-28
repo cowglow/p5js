dd04bb2e7ee14179a9845fc8416a31cf66ca8e3f
Philip Saa <cowglow@gmail.com>
2022-06-28 22:42:36 +0200

    New Sketch

--- src/sketches/prototype-003.ts
+++ src/sketches/prototype-003.ts
@@ -1,30 +1,45 @@
 import p5 from "p5";
-import { bootstrapCanvas } from "../lib/bootstrap-canvas";
+import {
+  bootstrapCanvas,
+  defaultCanvasDimensions,
+} from "../lib/bootstrap-canvas";
+import { bootstrapEventLister } from "../lib/bootstrap-event-lister";
 import VectorResource from "../classes/vector/vector";
 
-const palette = ["#bbe9c8", "#433650", "#67b9d5", "#fdc44e", "#c55148"];
-
+const palette = [
+  "187, 233, 200",
+  "67, 54, 80",
+  "103, 185, 213",
+  "253, 196, 78",
+  "197, 81, 72",
+];
+// @ts-ignore
 let vectorResource: VectorResource | undefined;
-let steps = 0;
-let maxStep = 80;
+let color = palette[Math.floor(Math.random() * palette.length)];
 
 export default (p: p5) => {
   bootstrapCanvas(p);
+  bootstrapEventLister(p, { filename: "prototype-003-" + Date.now() });
+
+  p.setup = () => {
+    const { canvasWidth, canvasHeight } = defaultCanvasDimensions;
+    p.createCanvas(canvasWidth, canvasHeight);
+    p.background(`rgba(${color}, 0.65)`);
+  };
 
   p.draw = () => {
-    const color = palette[Math.floor(Math.random() * palette.length)];
-    p.fill(color);
-
-    if (!vectorResource) {
-      if (steps < maxStep) {
-        vectorResource = new VectorResource(p, palette);
-        vectorResource._p5.rotate(steps % 120);
-        vectorResource.move(steps % 360)
-        p.ellipse(p.random(0, 1000), p.random(0, 1000), 80, 80);
-      }
-    } else {
-      vectorResource = undefined;
+    color = palette[Math.floor(Math.random() * palette.length)];
+    if (p.mouseIsPressed) {
+      let size = Math.floor(Math.random() * 80);
+      const v1 = p.createVector(window.innerWidth / 2, window.innerHeight / 2);
+      p.blendMode(p.LIGHTEST);
+      p.line(v1.x, v1.y, p.mouseX, p.mouseY);
+      p.fill(`rgba(${color}, 0.66)`);
+      p.ellipse(p.mouseX, p.mouseY, size, size);
+      p.stroke(color);
+      p.blendMode(p.ADD);
+      vectorResource = new VectorResource(p, palette);
     }
-    steps++;
   };
+
 };
