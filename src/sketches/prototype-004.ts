ff698fb31123583b51295d8a50b2ad84e174d25a
Philip Saa <cowglow@gmail.com>
2022-07-07 10:40:36 +0200

    Add color to prototype 004

--- src/sketches/prototype-004.ts
+++ src/sketches/prototype-004.ts
@@ -12,10 +12,17 @@ export default (p: p5) => {
   bootstrapEventLister(p, { filename: "prototype-004-" + Date.now() });
 
   p.draw = () => {
-    let random = Math.random();
-    p.stroke(255, random);
     p.strokeWeight(size);
-    if (random < 0.5) {
+
+    // Randomize color or non-color
+    if (Math.random() > 0.5) {
+      p.stroke(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), 255, Math.random());
+    } else {
+      p.stroke(255, Math.random())
+    }
+    
+    // Randomize direction
+    if (Math.random() < 0.5) {
       p.line(x, y, x + size, y + size);
     } else {
       p.line(x, y + size, x + size, y);
